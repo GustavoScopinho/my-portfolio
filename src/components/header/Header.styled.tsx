c99587d25a81f221c9d674618ceb066cc67f3e13
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2023-02-07 18:19:37 -0300

    Criando componente header

---
+++ src/components/header/Header.styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ContainerHeader = styled.div`
+  height: 70px;
+  width: 100%;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+`
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import { ContainerHeader } from './Header.styled'
+
+export const Header = () => {
+  return (
+    <>
+      <ContainerHeader>
+        <p>Sobre mim</p>
+        <p>Skills</p>
+        <p>Projetos</p>
+      </ContainerHeader>
+    </>
+  )
+}
